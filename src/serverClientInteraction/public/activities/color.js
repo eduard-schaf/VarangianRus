b5d114cbb4c9846195fa03842ab5fbe6d3b80973
eddi89 <eduard.schaf@web.de>
2017-02-02 20:47:17 +0100

    Add js doc for color.js

--- src/serverClientInteraction/public/activities/color.js
+++ src/serverClientInteraction/public/activities/color.js
@@ -1,4 +1,9 @@
 UI.color = {
+    /**
+     * Run the color activity.
+     *
+     * @param {string} topic the selected topic
+     */
     run: function (topic) {
         $(".hit").addClass("color-" + topic);
         $("#restore").show();
