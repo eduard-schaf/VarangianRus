ff02ce8655e0a35221644ee0f77612cbac6fb949
eddi89 <eduard.schaf@web.de>
2017-02-02 20:45:53 +0100

    Add js doc

--- src/serverClientInteraction/public/user-interface.js
+++ src/serverClientInteraction/public/user-interface.js
@@ -1,4 +1,9 @@
 const UI = {
+    /**
+     * Add text options to the text menu.
+     *
+     * @param {object} textNames contains all text names
+     */
     initTextMenuOptions: function (textNames) {
         const $TextMenu = $("#text-menu");
 
@@ -10,25 +15,43 @@ const UI = {
         }
     },
 
+    /**
+     * Initialize the text menu handler.
+     */
     initTextMenu: function () {
         $("#text-menu").on("change", UI.getText);
     },
 
+    /**
+     * Request to get the text from the server.
+     */
     getText: function () {
         $("#restore").hide();
         IO.socket.emit("get text", $("#text-menu").val());
     },
 
+    /**
+     * Add the text to the html page.
+     *
+     * @param {string} text the text with all data
+     */
     addText: function (text) {
         const $TextContent = $("#text-content");
         $TextContent.empty();
         $TextContent.append(text);
     },
 
+    /**
+     * Initialize the enhance button handler.
+     */
     initEnhance: function () {
         $("#enhance").on("click", UI.enhance);
     },
 
+    /**
+     * Enhance the text by first marking the hits and
+     * then running the selected topic.
+     */
     enhance: function () {
         const topic = $("#topic-menu").val();
         const activity = $("#activity-menu").val();
@@ -40,6 +63,9 @@ const UI = {
         }
     },
 
+    /**
+     * Mark the relevant tokens with the class 'hit'.
+     */
     markHits: function () {
         $("[data-part-of-speech='V-']").each(function () {
             const $Token =$(this);
@@ -52,15 +78,24 @@ const UI = {
         });
     },
 
+    /**
+     * Initialize the restore button handler.
+     */
     initRestore: function () {
         $("#restore").on("click", UI.restore);
     },
 
+    /**
+     * Initialize the restore button handler.
+     */
     restore: function () {
         $("token").removeAttr("class");
         $("#restore").hide();
     },
 
+    /**
+     * The ui state at the beginning.
+     */
     initialUiState: function () {
         $("#restore").hide();
         $(".menu").val("unselected");
