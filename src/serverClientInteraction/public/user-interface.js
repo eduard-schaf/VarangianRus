9a4368497713e6291fcce41b352023b9b902ff30
eddi89 <eduard.schaf@web.de>
2017-02-02 20:28:44 +0100

    Add user interface
    Now with topic and activity selection.

---
+++ src/serverClientInteraction/public/user-interface.js
@@ -0,0 +1,68 @@
+const UI = {
+    initTextMenuOptions: function (textNames) {
+        const $TextMenu = $("#text-menu");
+
+        for (const textKey in textNames) {
+            if (Object.prototype.hasOwnProperty.call(textNames, textKey)) {
+                const textName = textNames[textKey];
+                $TextMenu.append($("<option>").val(textKey).text(textName))
+            }
+        }
+    },
+
+    initTextMenu: function () {
+        $("#text-menu").on("change", UI.getText);
+    },
+
+    getText: function () {
+        $("#restore").hide();
+        IO.socket.emit("get text", $("#text-menu").val());
+    },
+
+    addText: function (text) {
+        const $TextContent = $("#text-content");
+        $TextContent.empty();
+        $TextContent.append(text);
+    },
+
+    initEnhance: function () {
+        $("#enhance").on("click", UI.enhance);
+    },
+
+    enhance: function () {
+        const topic = $("#topic-menu").val();
+        const activity = $("#activity-menu").val();
+
+        if("verbs-aorist-tense" === topic){
+            UI.addHits();
+
+            UI[activity].run(topic);
+        }
+    },
+
+    addHits: function () {
+        $("[data-part-of-speech='V-']").each(function () {
+            const $Token =$(this);
+            const morphology = $Token.data("morphology");
+            const tense = morphology.charAt(2);
+
+            if("a" === tense){
+                $Token.addClass("hit");
+            }
+        });
+    },
+
+    initRestore: function () {
+        $("#restore").on("click", UI.restore);
+    },
+
+    restore: function () {
+        $("token").removeAttr("class");
+        $("#restore").hide();
+    },
+
+    initialUiState: function () {
+        $("#restore").hide();
+        $(".menu").val("unselected");
+    }
+};
