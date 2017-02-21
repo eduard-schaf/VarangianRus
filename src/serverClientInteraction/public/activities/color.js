a44bde19a2507f235c66d0967d584f4c1449c8b0
eddi89 <eduard.schaf@web.de>
2017-02-21 22:14:49 +0100

    Make color style uniform among topics

--- src/serverClientInteraction/public/activities/color.js
+++ src/serverClientInteraction/public/activities/color.js
@@ -3,6 +3,6 @@ UI.color = {
    * Run the color activity.
    */
   run: function() {
-    $(".hit").addClass("color-style-" + $("#topic-menu").val());
+    $(".hit").addClass("color-style");
   }
 };
