d382d31892f6645d4d0b36407b8d55b70d0d7636
eddi89 <eduard.schaf@web.de>
2017-02-22 00:12:06 +0100

    Add activities to topic data

--- src/serverClientInteraction/public/data.js
+++ src/serverClientInteraction/public/data.js
@@ -1,23 +1,27 @@
 UI.data = {
   "verbs-aorist-tense": {
+    "activities": "color,click,mc,cloze",
     "part-of-speech": "V-",
     "feature": "a",
     "feature-position": 2
   },
 
   "verbs-imperfect-tense": {
+    "activities": "color,click,mc,cloze",
     "part-of-speech": "V-",
     "feature": "i",
     "feature-position": 2
   },
 
   "nouns-vocative": {
+    "activities": "color,click,mc",
     "part-of-speech": "Ne,Nb",
     "feature": "v",
     "feature-position": 6
   },
 
   "nouns-dual": {
+    "activities": "color,click,mc",
     "part-of-speech": "Ne,Nb",
     "feature": "d",
     "feature-position": 1
