e58f8c70b5a67af0a988846551a6d0db7a992ea9
eddi89 <eduard.schaf@web.de>
2017-02-22 22:13:14 +0100

    Add verbs active present participles

--- src/serverClientInteraction/public/data.js
+++ src/serverClientInteraction/public/data.js
@@ -2,28 +2,35 @@ UI.data = {
   "verbs-aorist-tense": {
     "activities": "color,click,mc,cloze",
     "part-of-speech": "V-",
-    "feature": "a",
-    "feature-position": 2
+    "features": "a",
+    "feature-positions": "2"
   },
 
   "verbs-imperfect-tense": {
     "activities": "color,click,mc,cloze",
     "part-of-speech": "V-",
-    "feature": "i",
-    "feature-position": 2
+    "features": "i",
+    "feature-positions": "2"
+  },
+
+  "verbs-active-present-participles": {
+    "activities": "color,click,mc",
+    "part-of-speech": "V-",
+    "features": "ppa",
+    "feature-positions": "2,3,4"
   },
 
   "nouns-vocative": {
     "activities": "color,click,mc",
     "part-of-speech": "Ne,Nb",
-    "feature": "v",
-    "feature-position": 6
+    "features": "v",
+    "feature-positions": "6"
   },
 
   "nouns-dual": {
     "activities": "color,click,mc",
     "part-of-speech": "Ne,Nb",
-    "feature": "d",
-    "feature-position": 1
+    "features": "d",
+    "feature-positions": "1"
   }
 };
