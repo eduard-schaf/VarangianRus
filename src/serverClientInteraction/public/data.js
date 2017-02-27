733296c3373c84e33f8a322ad93b7952437d0b1e
eddi89 <eduard.schaf@web.de>
2017-02-27 22:31:27 +0100

    Add new topics data

--- src/serverClientInteraction/public/data.js
+++ src/serverClientInteraction/public/data.js
@@ -151,5 +151,185 @@ UI.data = {
     "part-of-speech": "A-",
     "features": "smn",
     "feature-positions": "1,5,6"
+  },
+
+  "adjectives-nominative-singular-feminine": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "sfn",
+    "feature-positions": "1,5,6"
+  },
+
+  "adjectives-nominative-plural": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "pn",
+    "feature-positions": "1,6"
+  },
+
+  "adjectives-genitive-singular-masculine": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "smg",
+    "feature-positions": "1,5,6"
+  },
+
+  "adjectives-genitive-singular-feminine": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "sfg",
+    "feature-positions": "1,5,6"
+  },
+
+  "adjectives-genitive-plural": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "pg",
+    "feature-positions": "1,6"
+  },
+
+  "adjectives-dative-singular-masculine": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "smd",
+    "feature-positions": "1,5,6"
+  },
+
+  "adjectives-dative-singular-feminine": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "sfd",
+    "feature-positions": "1,5,6"
+  },
+
+  "adjectives-dative-plural": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "pd",
+    "feature-positions": "1,6"
+  },
+
+  "adjectives-accusative-singular-masculine": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "sma",
+    "feature-positions": "1,5,6"
+  },
+
+  "adjectives-accusative-singular-feminine": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "sfa",
+    "feature-positions": "1,5,6"
+  },
+
+  "adjectives-accusative-plural": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "pa",
+    "feature-positions": "1,6"
+  },
+
+  "adjectives-instrumental-singular-masculine": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "smi",
+    "feature-positions": "1,5,6"
+  },
+
+  "adjectives-instrumental-singular-feminine": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "sfi",
+    "feature-positions": "1,5,6"
+  },
+
+  "adjectives-instrumental-plural": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "pi",
+    "feature-positions": "1,6"
+  },
+
+  "adjectives-locative-singular-masculine": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "sml",
+    "feature-positions": "1,5,6"
+  },
+
+  "adjectives-locative-singular-feminine": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "sml",
+    "feature-positions": "1,5,6"
+  },
+
+  "adjectives-locative-plural": {
+    "activities": "color,click,mc",
+    "part-of-speech": "A-",
+    "features": "pl",
+    "feature-positions": "1,6"
+  },
+
+  "personal-pronouns-third-singular-masculine": {
+    "activities": "color,click,mc",
+    "part-of-speech": "Pp",
+    "features": "3sm",
+    "feature-positions": "0,1,5"
+  },
+
+  "personal-pronouns-third-singular-feminine": {
+    "activities": "color,click,mc",
+    "part-of-speech": "Pp",
+    "features": "3sf",
+    "feature-positions": "0,1,5"
+  },
+
+  "personal-pronouns-third-singular-neuter": {
+    "activities": "color,click,mc",
+    "part-of-speech": "Pp",
+    "features": "3sn",
+    "feature-positions": "0,1,5"
+  },
+
+  "personal-pronouns-third-dual": {
+    "activities": "color,click,mc",
+    "part-of-speech": "Pp",
+    "features": "3d",
+    "feature-positions": "0,1"
+  },
+
+  "personal-pronouns-third-plural": {
+    "activities": "color,click,mc",
+    "part-of-speech": "Pp",
+    "features": "3p",
+    "feature-positions": "0,1"
+  },
+
+  "demonstrative-pronouns-masculine": {
+    "activities": "color,click",
+    "part-of-speech": "Pd",
+    "features": "m",
+    "feature-positions": "5"
+  },
+
+  "demonstrative-pronouns-feminine": {
+    "activities": "color,click",
+    "part-of-speech": "Pd",
+    "features": "f",
+    "feature-positions": "5"
+  },
+
+  "demonstrative-pronouns-neuter": {
+    "activities": "color,click",
+    "part-of-speech": "Pd",
+    "features": "d",
+    "feature-positions": "5"
+  },
+
+  "relative-pronouns": {
+    "activities": "color,click",
+    "part-of-speech": "Pr",
   }
 };
